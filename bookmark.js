4d43365e85e5b14637536f168069daeffe40d59a
moshmage <moshmage@gmail.com>
2014-05-21 14:11:31 +0100

    on the works to add a "save in the cloud" button

--- bookmark.js
+++ bookmark.js
@@ -1,7 +1,7 @@
 var bookmarks = angular.module('bookmarks',[]),
 	uid = 0;
 
-bookmarks.controller('bmControl',['$scope', function($scope) {
+bookmarks.controller('bmControl',['$scope','$http', function($scope, $http) {
 	$scope.bookmark = {};
 	if (localStorage.bookmarks) {
 		$scope.links = angular.fromJson(localStorage.bookmarks);
@@ -16,12 +16,29 @@ bookmarks.controller('bmControl',['$scope', function($scope) {
 		$scope.links.push($scope.bookmark);
 		localStorage.bookmarks = angular.toJson($scope.links);
 		$scope.newlink = "";
-
 	}
 	$scope.delLink = function(linkId) {
 		console.log("deleting link "+linkId+" with value: "+$scope.links[linkId].link);
 		$scope.links.splice(linkId,1);
 		localStorage.bookmarks = angular.toJson($scope.links);
 	}
-
+	$scope.saveBookmarks = function() {
+		var dataToPost = {
+			'paste_data':localStorage.bookmarks,
+			'paste_lang':'text',
+			'api_submit':true,
+			'mode':'json',
+			'paste_expire':0
+		};
+		$http.jsonp('http://paste.is/',dataToPost).success(function(data,status,headers,config) {
+			console.log(data);
+			console.log(status);
+			console.log(headers);
+			console.log(config);
+		}).error(function(data,status){
+			console.log("ERROR GETTING THING:");
+			console.log(data);
+			console.log(status);
+		});
+	}
 }]);
